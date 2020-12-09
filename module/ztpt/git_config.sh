688d3da716324ae988e4d790f3260b6127ac597b
eduryev <edwardduriev@gmail.com>
2020-12-09 12:47:46 +0000

    git_config for module shortcut

---
+++ module/ztpt/git_config.sh
@@ -0,0 +1,7 @@
+cd /content/gdrive/MyDrive/ybshmmlchk/zatoboj
+mkdir -p ~/.ssh
+cp ../id_rsa ~/.ssh
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+ssh -T git@github.com
+source ../git_config_startup.sh
+git pull
